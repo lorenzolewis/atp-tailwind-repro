abd27572d387d8720035d80b50263bf2132f3144
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2025-03-24 22:04:07 +0100

    add tailwind

--- package/index.ts
+++ package/index.ts
@@ -1,10 +1,25 @@
 import defineTheme from "astro-theme-provider";
 import { z } from "astro/zod";
+import { type AstroIntegration } from "astro";
+import tailwindcss from "@tailwindcss/vite";
+
+function integration(): AstroIntegration {
+  return {
+    name: "my-integration",
+    hooks: {
+      "astro:config:setup"({ updateConfig }) {
+        // @ts-expect-error
+        updateConfig({ vite: { plugins: [tailwindcss()] } });
+      },
+    },
+  };
+}
 
 export default defineTheme({
-	name: "my-theme",
-	schema: z.object({
-		title: z.string(),
-		description: z.string().optional(),
-	}),
+  name: "my-theme",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+  }),
+  integrations: [integration()],
 });
